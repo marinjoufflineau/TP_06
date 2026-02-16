4e74b86468599d6c82d987dcf3b2432edd5cf369
Marinho <marin.joufflineau@gmail.com>
2026-02-16 10:12:20 +0100

    init spring boot

---
+++ src/main/java/com/tp06/tp06/Tp06Application.java
@@ -0,0 +1,13 @@
+package com.tp06.tp06;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp06Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp06Application.class, args);
+	}
+
+}
